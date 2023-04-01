b78cf8c14f21e4ce027d332f46f8835e012f6c55
poom-sci <poom.suchaoin@gmail.com>
2023-04-01 17:31:41 +0700

    Update main.go

--- main.go
+++ main.go
@@ -5,22 +5,28 @@ import (
 	"github.com/gofiber/fiber"
 )
 
-var (
-	app *fiber.App
-)
+// var (
+// 	app *fiber.App
+// )
 
-func init() {
-	app = fiber.New()
+// func init() {
+// 	app = fiber.New()
+
+// 	app.Get("/", func(c *fiber.Ctx) {
+// 		c.Send("Hello, World!")
+// 	})
+
+// 	// app.Listen(3000)
+// }
+
+// ADD THIS SCRIPT
+func Handler(c *fiber.Ctx) {
+	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) {
 		c.Send("Hello, World!")
 	})
 
-	app.Listen(3000)
-}
-
-// ADD THIS SCRIPT
-func Handler(c *fiber.Ctx) {
 	adaptor.HTTPHandler(app).ServeHTTP(c)
 
 }
