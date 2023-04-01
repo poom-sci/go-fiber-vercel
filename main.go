c59fd0e5cfeea6fcaf04e07a63cf55bfc3cdeb94
poom-sci <poom.suchaoin@gmail.com>
2023-04-01 19:20:37 +0700

    add middleware

--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/csrf"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		Prefork:       true,
+		CaseSensitive: true,
+		StrictRouting: true,
+	})
+
+	app.Use(compress.New())
+	app.Use(cors.New())
+	app.Use(csrf.New())
+	// app.Use(cache.New())
+
+	// recover from panic
+	app.Use(recover.New())
+
+	app.Use(logger.New())
+
+	app.Get("/metrics", monitor.New(monitor.Config{Title: "MyService Metrics Page"}))
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
@@ -19,4 +42,6 @@ func main() {
 	}
 
 	app.Listen("0.0.0.0:" + port)
+
+	log.Fatal(app.Listen(":" + port))
 }
