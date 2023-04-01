6da8db05cfd42b2f8e0811b052d23c7aa8645b0d
poom-sci <poom.suchaoin@gmail.com>
2023-04-01 17:52:23 +0700

    test

--- api/index.go
+++ api/index.go
@@ -1,9 +1,11 @@
+
 package handler
 
 import (
 	"fmt"
 	"net/http"
-	// . "github.com/tbxark/g4vercel"
+
+	. "github.com/tbxark/g4vercel"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
