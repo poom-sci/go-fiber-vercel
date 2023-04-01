6ce1112e1aebe08e678d65bf617600f38b4919a6
poom-sci <poom.suchaoin@gmail.com>
2023-04-01 17:16:13 +0700

    Update main.go

--- main.go
+++ main.go
@@ -6,12 +6,21 @@ import (
 	"github.com/gofiber/fiber"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	app := fiber.New()
+var (
+	app *fiber.App
+)
+
+func main() {
+	app = fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) {
 		c.Send("Hello, World!")
 	})
 
-	app.Listen(3000)
+	// app.Listen(8080)
+}
+
+// ADD THIS SCRIPT
+func Handler(w http.ResponseWriter, r *http.Request) {
+	app.Listen(8080)
 }
